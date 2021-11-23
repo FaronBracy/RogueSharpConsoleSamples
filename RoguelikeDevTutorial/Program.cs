1c2db8b5cecc4c398b9d475258714c9f424758e2
Faron Bracy <signup@dreamersdesign.com>
2021-11-23 10:55:25 -0500

    + make modifiers explicit and format main

--- RoguelikeDevTutorial/Program.cs
+++ RoguelikeDevTutorial/Program.cs
@@ -2,11 +2,11 @@
 
 namespace RoguelikeDevTutorial
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   public class Program
+   {
+      public static void Main( string[] args )
+      {
+         Console.WriteLine( "Hello World!" );
+      }
+   }
 }

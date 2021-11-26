fbe07fcdefabeb4fa1db49e3583785f2a1c1d393
Faron Bracy <signup@dreamersdesign.com>
2021-11-26 15:41:29 -0500

    Keep track of player position and start centered

--- RoguelikeDevTutorial/Program.cs
+++ RoguelikeDevTutorial/Program.cs
@@ -13,6 +13,9 @@ namespace RoguelikeDevTutorial
          int screenWidth = 80;
          int screenHeight = 50;
 
+         int playerX = screenWidth / 2;
+         int playerY = screenHeight / 2;
+
          // BitmapFont in RogueSharp is like the tileset in TCOD
          BitmapFont tileset = new BitmapFont( 10, 10, 32, 8, "dejavu10x10_gs_tc.png", BitmapFontLayout.Tcod );
 
@@ -28,7 +31,7 @@ namespace RoguelikeDevTutorial
          MainWindow.Render += MainWindowRender;
          MainWindow.Update += MainWindowUpdate;
 
-         MainWindow.RootConsole.Print( 1, 1, "@", RSColor.White );  
+         MainWindow.RootConsole.Print( playerX, playerY, "@", RSColor.White );  
 
          // Kick off the main game loop
          MainWindow.Start();

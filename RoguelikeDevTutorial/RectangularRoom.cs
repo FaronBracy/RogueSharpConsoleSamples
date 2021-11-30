ec50ccb12c8a8a2bea8d7a8c40114816b3961c5a
Faron Bracy <signup@dreamersdesign.com>
2021-11-29 19:47:21 -0500

    + Rectangular room class

---
+++ RoguelikeDevTutorial/RectangularRoom.cs
@@ -0,0 +1,38 @@
+﻿using RogueSharp;
+
+namespace RoguelikeDevTutorial
+{
+   public class RectangularRoom
+   {
+      public int X1 { get; set; }
+      public int Y1 { get; set; }
+      public int X2 { get; set; }
+      public int Y2 { get; set; }
+
+      public Point Center
+      {
+         get
+         {
+            int centerX = ( X1 + X2 ) / 2;
+            int centerY = ( Y1 + Y2 ) / 2;
+            return new Point( centerX, centerY );
+         }
+      }
+      public (Point, Point) Inner
+      {
+         get
+         {
+            Point xRange = new Point( X1 + 1, X2 );
+            Point yRange = new Point( Y1 + 1, Y2 );
+            return (xRange, yRange);
+         }
+      }
+      public RectangularRoom( int x1, int y1, int x2, int y2 )
+      {
+         X1 = x1;
+         Y1 = y1;
+         X2 = x2;
+         Y2 = y2;
+      }
+   }
+}

f232806f5f42b1ee85cff7f44798a9dca412be3b
Faron Bracy <signup@dreamersdesign.com>
2023-02-18 12:19:05 -0500

    Refactor Rectangular room class

--- RoguelikeDevTutorial/RectangularRoom.cs
+++ RoguelikeDevTutorial/RectangularRoom.cs
@@ -4,32 +4,20 @@ namespace RoguelikeDevTutorial
 {
    public class RectangularRoom
    {
-      public int X1 { get; }
-      public int Y1 { get; }
-      public int Width { get; }
-      public int Height { get; }
+      public int Left => Outer.Left;
+      public int Top => Outer.Top;
+      public int Width => Outer.Width;
+      public int Height => Outer.Height;
 
-      public int X2 => X1 + Width;
-      public int Y2 => Y1 + Height;
+      public Rectangle Outer { get; }
 
       public RectangularRoom( int xLeft, int yTop, int width, int height )
       {
-         X1 = xLeft;
-         Y1 = yTop;
-         Width = width;
-         Height = height;
+         Outer = new Rectangle( xLeft, yTop, width, height );
       }
 
-      public Point Center
-      {
-         get
-         {
-            int centerX = ( X1 + X2 ) / 2;
-            int centerY = ( Y1 + Y2 ) / 2;
-            return new Point( centerX, centerY );
-         }
-      }
+      public Point Center => Outer.Center;
 
-      public Rectangle Inner => new Rectangle(X1 + 1, Y1 + 1, Width - 2, Height - 2 );
+      public Rectangle Inner => new(Left + 1, Top + 1, Width - 2, Height - 2 );
    }
 }

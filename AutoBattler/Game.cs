f352be28c4fb321bf3dd41f09f89d28f202195c4
Faron Bracy <signup@dreamersdesign.com>
2023-11-05 15:21:29 -0500

    Render @ sign

--- AutoBattler/Game.cs
+++ AutoBattler/Game.cs
@@ -1,9 +1,24 @@
-﻿namespace AutoBattler;
+﻿using RogueSharp.ConsoleEngine;
+
+namespace AutoBattler;
 
 public static class Game
 {
-    public static void Main()
-    {
-        Console.WriteLine("Hello World!");
-    }
+   public static RSWindow MainWindow { get; private set; }
+
+   public static void Main()
+   {
+      BitmapFont bitmapFont = new BitmapFont( 10, 10, 16, 16, "qbicfeet_10x10.png", BitmapFontLayout.Cp437 );
+      MainWindow = new RSWindow( bitmapFont, 100, 50, "Auto Battler" );
+      MainWindow.Render += MainWindowRender;
+      MainWindow.Start();
+      Console.WriteLine( "Hello World!" );
+   }
+
+   private static void MainWindowRender( object? sender, FrameEventArgs e )
+   {
+      MainWindow.RootConsole.Clear();
+      MainWindow.RootConsole.SetChar( 5, 5, '@' );
+      MainWindow.Draw();
+   }
 }

4a41150e0d595db069c83472bc99027d0d6f5532
Faron Bracy <signup@dreamersdesign.com>
2023-11-07 18:56:31 -0500

    Basic animation class

---
+++ AutoBattler/Animation.cs
@@ -0,0 +1,33 @@
+﻿using RogueSharp.ConsoleEngine;
+
+namespace AutoBattler
+{
+   public class Animation
+   {
+      public RSColor EndColor { get; set; }
+      public RSColor StartColor { get; set; }
+      public long DurationMs { get; set; }
+
+      public long StartTimeMs { get; set; }
+      public RSColor CurrentColor { get; set; }
+
+      public Animation( long durationMs, RSColor startColor, RSColor endColor )
+      {
+         DurationMs = durationMs;
+         StartColor = startColor;
+         EndColor = endColor;
+         StartTimeMs = Game.MainWindow.Stopwatch.ElapsedMilliseconds;
+      }
+
+      public void Update( FrameEventArgs e )
+      {
+         double percentComplete = ( (double) e.TotalElapsedMs - StartTimeMs ) / DurationMs;
+         CurrentColor = RSColor.Blend( StartColor, EndColor, (float) percentComplete );
+      }
+
+      public void Render( FrameEventArgs e )
+      {
+         Game.MainWindow.RootConsole.SetBackColor( 5, 5, CurrentColor );
+      }
+   }
+}
--- AutoBattler/Game.cs
+++ AutoBattler/Game.cs
@@ -5,26 +5,41 @@ namespace AutoBattler;
 public static class Game
 {
    public static RSWindow MainWindow { get; private set; }
+   public static List<Animation> Animations = new List<Animation>();
 
    public static void Main()
    {
       BitmapFont bitmapFont = new BitmapFont( 10, 10, 16, 16, "qbicfeet_10x10.png", BitmapFontLayout.Cp437 );
+
       MainWindow = new RSWindow( bitmapFont, 100, 50, "Auto Battler" );
       MainWindow.Update += MainWindowUpdate;
       MainWindow.Render += MainWindowRender;
       MainWindow.KeyDown += MainWindowKeyDown;
       MainWindow.Quitting += MainWindowQuitting;
+
+      Animation animation = new Animation( 10000, RSColor.White, RSColor.Red );
+      Animations.Add( animation );
+
       MainWindow.Start();
    }
 
    private static void MainWindowUpdate( object? sender, FrameEventArgs e )
    {
+      foreach ( Animation animation in Animations )
+      {
+         animation.Update( e );
+      }
    }
 
    private static void MainWindowRender( object? sender, FrameEventArgs e )
    {
       MainWindow.RootConsole.Clear();
       MainWindow.RootConsole.SetChar( 5, 5, '@' );
+      foreach ( Animation animation in Animations )
+      {
+         // TODO: After animation is complete, remove it from the list
+         animation.Render( e );
+      }
       MainWindow.Draw();
    }
 

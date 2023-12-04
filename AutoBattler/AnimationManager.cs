63914a53d8d8f40c37f3e98fbab71d88f6818e3d
Faron Bracy <signup@dreamersdesign.com>
2023-12-04 10:14:54 -0500

    Remove unused code

--- AutoBattler/AnimationManager.cs
+++ AutoBattler/AnimationManager.cs
@@ -44,47 +44,4 @@ namespace AutoBattler
          }
       }
    }
-
-   public static class AnimationManager
-   {
-      public static List<Animation> Animations = new List<Animation>();
-
-      public static void AddAnimation(long startOffsetMs, Animation animation )
-      {
-         animation.StartTimeMs = Game.MainWindow.ElapsedMilliseconds + startOffsetMs;
-         //Console.WriteLine( $"Adding Animation at {animation.StartTimeMs}" );
-         Animations.Add( animation );
-      }
-
-      public static void RemoveAnimation( Animation animation )
-      {
-         // TODO: Expire the animation so it will remove on the next update
-         //Console.WriteLine( "Removing Animation" );
-         Animations.Remove( animation );
-      }
-
-      public static void Update( FrameEventArgs e )
-      {
-         foreach ( Animation animation in Animations )
-         {
-            if ( animation.StartTimeMs <= Game.MainWindow.ElapsedMilliseconds )
-            {
-               animation.Update( e );
-            }
-         }
-
-         Animations.RemoveAll( animation => animation.IsComplete );
-      }
-
-      public static void Render( FrameEventArgs e )
-      {
-         foreach ( Animation animation in Animations )
-         {
-            if ( animation.StartTimeMs <= Game.MainWindow.ElapsedMilliseconds )
-            {
-               animation.Render( e );
-            }
-         }
-      }
-   }
 }
--- AutoBattler/Game.cs
+++ AutoBattler/Game.cs
@@ -27,21 +27,12 @@ public static class Game
       {
          Map.SetCellProperties( cell.X, cell.Y, true, true );
       }
-
-
-      //for ( int x = 5; x < 50; x++ )
-      //{
-      //   Animation animation = new Animation( 1000, new RSColor(1, 0, 0), new RSColor(255,0,0), x, 5 );
-      //   AnimationManager.AddAnimation( (x - 4) * 10, animation );
-      //}
-
-
+      
       MainWindow.Start();
    }
 
    private static void MainWindowUpdate( object? sender, FrameEventArgs e )
    {
-      //AnimationManager.Update( e );
       AnimationSystem.Update( e );
    }
 
@@ -56,7 +47,6 @@ public static class Game
 
       MainWindow.RootConsole.SetChar( 10, 10, '\u2192' );
       MainWindow.RootConsole.SetChar( 11, 10, BitmapFont.TileIndexToUnicodeInt( 27 ) );
-      //AnimationManager.Render( e );
       AnimationSystem.Render( e );
       MainWindow.Draw();
    }
@@ -70,9 +60,6 @@ public static class Game
       // TODO - Get symbol animation working
 
       Console.WriteLine( e );
-      //Effects.ShootArrow( new Point( 5, 5 ), new Point( Mouse.X, Mouse.Y ) );
-      //Effects.Explosion( new Point( Mouse.X, Mouse.Y ), 7 );
-
 
       //AnimationSeries animationSeries = new AnimationSeries();
 
@@ -91,20 +78,6 @@ public static class Game
 
       AnimationSystem.AddAnimations( lineAnimationGroup.GetAnimations() );
       AnimationSystem.AddAnimations( circleAnimationGroup.GetAnimations(), lineAnimationGroup.AnimationGroupLengthMs );
-
-      //CellAnimation cellAnimation = new CellAnimation()
-      //   .At( Mouse.X, Mouse.Y )
-      //   .WithDuration( 2500 )
-      //   .WithBackgroundColorAnimation( RSColor.Yellow, RSColor.Red );
-
-      //AnimationSystem.AddAnimation( cellAnimation );
-
-      //CircleAnimation circleAnimation = new CircleAnimation( 1000, 50, new Point( Mouse.X, Mouse.Y )
-      //   , 5, RSColor.Yellow, new RSColor( 255, 0, 0 ) );
-      //circleAnimation.BeginSolid();
-      //LineAnimation lineAnimation = new LineAnimation( 1000, 10, new Point( 5, 5 ), new Point( Mouse.X, Mouse.Y )
-      //   , new RSColor( 255, 0, 0 ), new RSColor( 1, 0, 0 ), '*' );
-      //lineAnimation.Begin();
    }
 
    private static void MainWindowMouseMove( object? sender, MouseEventArgs e )
